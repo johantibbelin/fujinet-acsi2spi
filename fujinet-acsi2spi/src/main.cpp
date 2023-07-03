a489c90c4d2ca6596629a7833908b88cba9383c7
Johan Tibbelin <johan.roos@tibbelin.com>
2023-07-03 17:21:03 +0200

    Update main.cpp

--- fujinet-acsi2spi/src/main.cpp
+++ fujinet-acsi2spi/src/main.cpp
@@ -1,17 +1,24 @@
 #include <Arduino.h>
-#include <stdlib.h>
-#include "pinmap.h"
+//#include "pinmap.h"
+
+const uint LED_PIN = 25;
 
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+ // Serial.begin(9600);
+  gpio_set_dir(LED_BUILTIN, GPIO_OUT);
+ // Serial.println("Fujinet acsi2spi started...");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  gpio_put(LED_BUILTIN,1);
+  sleep_ms(250);
+  gpio_put(LED_BUILTIN,0);
+  sleep_ms(250);
 }
 
 // put function definitions here:
